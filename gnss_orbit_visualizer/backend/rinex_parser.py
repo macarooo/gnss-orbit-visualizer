edbef1b3462406e913cf91542365801d5b0cffa0
macarooo <macarooo@foxmail.com>
2026-05-15 10:04:14 +0800

    feat: push all 13 project files to gnss_orbit_visualizer

--- gnss_orbit_visualizer/backend/rinex_parser.py
+++ gnss_orbit_visualizer/backend/rinex_parser.py
@@ -1,6 +1,1 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""Generic RINEX parser"""
-def parse_rinex_obs(filepath):
-    with open(filepath, 'r', encoding='latin-1') as f:
-        return {'data': f.read()}
+placeholder
